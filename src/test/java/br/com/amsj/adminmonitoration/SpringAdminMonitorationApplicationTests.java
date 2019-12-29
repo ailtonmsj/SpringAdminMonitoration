f69abe2d5589388d063cc218d265e9064ef9b3cf
Ailton Muniz Santos Junior <ailton.msj@gmail.com>
2019-12-29 17:10:04 -0300

    First commit

---
+++ src/test/java/br/com/amsj/adminmonitoration/SpringAdminMonitorationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.amsj.adminmonitoration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdminMonitorationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
